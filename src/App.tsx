5a76244b706b251cde03929fe925643f1f83f5af
Pavel Liashkevich <paul8paul8@mail.ru>
2024-01-08 21:25:14 +0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { Footer } from './layout/footer/Footer'
 import { Header } from './layout/header/Header'
 import { CreateNft } from './layout/sections/createNft/CreateNft'
